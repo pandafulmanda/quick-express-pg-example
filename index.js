d57f8961eab1ecb45a73c5ba5a7860a965fad6e9
amanda <pandafulmanda@gmail.com>
2018-12-22 11:09:37 -0600

    server cleaned up

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const queries = require('./queries')
 
 const app = express()
 
@@ -7,11 +8,16 @@ app.get('/api/song/:title', handleSongAPIRoute)
 function handleSongAPIRoute(req, res, nextFn) {
   const title = req.params.title
 
-  db.query(getSongQuery, title)
+  queries.getSongByTitle(title)
     .then(function (result) {
       res.json(result)
     })
+    .catch(function () {
+      res.status(404)
+    })
 }
+
+
 
 app.listen(3000, function () {
   console.log('The server is up and running! at port 3000')
